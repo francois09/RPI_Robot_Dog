a88824d1e04fe916c948d5883b45fff80ff8bdd7
François TOURDE <fr-github@tourde.org>
2025-12-22 18:50:01 +0100

    Feat: Led show progression of startup
    Use leds for indicating startup progression

--- Code/RobotServer.sh
+++ Code/RobotServer.sh
@@ -2,10 +2,13 @@
 
 # Update repository with recent version
 cd /opt/RPI_Robot_Dog
+sudo python Code/Server/Progress.py fetch
 git fetch origin
+sudo python Code/Server/Progress.py pull
 git pull
 
 # Then prepare and run the server
+sudo python Code/Server/Progress.py start
 cd Code/Server
 cp ~/point.txt .
 cp ~/params.json .
